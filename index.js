1895c3ee49b274e55abb02928a95fe11c06e1f0b
gpsnts <gpsnts@protonmail.com>
2025-07-13 09:45:21 -0300

    Testing secrets

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const DB_USER = process.env.DB_USER || "error fetching: DB_USER";
+const DB_PORT = process.env.DB_PORT || "error fetching: DB_PORT";
+const DB_PASSWORD = process.env.DB_PASSWORD || "error fetching: DB_PASSWORD";
+const DB_HOST = process.env.DB_HOST || "error fetching: DB_HOST";
 
 app.get('/', (req, res) => {
 	res.send('<h1>Hello World!</h1>');
@@ -8,4 +12,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server on port: ${port}`);
+  console.log(`DB_USER: ${DB_USER}\nDB_PORT: ${DB_PORT}\nDB_PASSWORD: ${DB_PASSWORD}\nDB_HOST: ${DB_HOST}`)
 });
