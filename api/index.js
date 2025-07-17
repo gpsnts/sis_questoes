bed8c0b192cfcb2065b658a7b9f9b105dfc41fea
gpsnts <gpsnts@protonmail.com>
2025-07-16 22:36:46 -0300

    Vercel Test

--- api/index.js
+++ api/index.js
@@ -6,13 +6,16 @@ const DB_PORT = process.env.DB_PORT || "ERROR";
 const DB_PASSWORD = process.env.DB_PASSWORD || "ERROR";
 const DB_HOST = process.env.DB_HOST || "ERROR";
 
-app.get('/', (req, res) => {
-	res.send('<h1>Hello World!</h1>');
+app.get("/", (req, res) => {
+	res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/envs", (req, res) => {
+  res.send(`DB_USER: ${DB_USER}\nDB_PORT: ${DB_PORT}\nDB_PASSWORD: ${DB_PASSWORD}\nDB_HOST: ${DB_HOST}`);
 });
 
 app.listen(port, () => {
   console.log(`Server on port: ${port}`);
-  console.log(`DB_USER: ${DB_USER}\nDB_PORT: ${DB_PORT}\nDB_PASSWORD: ${DB_PASSWORD}\nDB_HOST: ${DB_HOST}`)
 });
 
 module.exports = app;
